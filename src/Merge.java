f6ef5b76f714ffb9ac0a1e594f7c95d17749a28d
AnzilottiChristopher <anzilottichr@gmail.com>
2023-02-21 19:14:16 -0500

    Added Hello branch print line

--- src/Merge.java
+++ src/Merge.java
@@ -3,5 +3,6 @@ public class Merge
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Hello, World!");
+        System.out.println("Hello branch!");
     }
 }
